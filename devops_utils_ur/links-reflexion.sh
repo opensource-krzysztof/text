650938470ead0e8c5c814cffa527acdc99e98579
rbanka1 <renata.banka@intel.com>
2025-04-15 12:21:58 +0200

    igc-cm

--- devops_utils_ur/links-reflexion.sh
+++ devops_utils_ur/links-reflexion.sh
@@ -1,24 +1,17 @@
-<<<<<<< HEAD:devops_utils_ur/links-reflexion.sh
 download-dpc.txt - https://tinyurl.com/devops-rk-1
 build.txt - https://tinyurl.com/devops-rk-2
 configure-ur.txt - https://tinyurl.com/devops-rk-3
 test_adapter_specific.txt - https://tinyurl.com/devops-rk-4
-=======
-Download DPC.txt - https://tinyurl.com/devops-rk-1
-Build.txt - https://tinyurl.com/devops-rk-2
-cmake_ur.txt - https://tinyurl.com/devops-rk-3
-Test_adapter_specific.txt - https://tinyurl.com/devops-rk-4
->>>>>>> bbd5d5157f049aae7b8f51ca29d1ce927e412fdd:links-reflexion.txt
 check_sycl-ls.txt - https://tinyurl.com/devops-rk-5
 install.txt - https://tinyurl.com/devops-rk-6
 test_adapters.txt - https://tinyurl.com/devops-rk-7
 alldeps.txt https://tinyurl.com/devops-rk-8
 opencl.txt https://tinyurl.com/devops-rk-9
 single_test.sh https://tinyurl.com/devops-rk-10
-<<<<<<< HEAD:devops_utils_ur/links-reflexion.sh
 pythonConfig.txt https://tinyurl.com/devops-rk-11
 managed.txt https://tinyurl.com/devops-rk-12
-=======
-python.txt https://tinyurl.com/devops-rk-11
-managed.txt https://tinyurl.com/devops-rk-12
->>>>>>> bbd5d5157f049aae7b8f51ca29d1ce927e412fdd:links-reflexion.txt
+igc-cm 1.0-119.u20.04 latest https://tinyurl.com/devops-rk-14
+igc-cm 1.0.120+i1_u20.04 https://tinyurl.com/devops-rk-15
+igc-cm 1.0-97.u20.04 https://tinyurl.com/devops-rk-16
+igc-cm 1.0.126+i1_u20.04 https://tinyurl.com/devops-rk-17
+igc-cm 1.0.91.u30.04 https://tinyurl.com/devops-rk-18
